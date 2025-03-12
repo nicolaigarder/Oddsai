66fbf6cfc27666694ea88633e1357ba6f21349bc
nicolaigarder <s224186@student.dtu.dk>
2025-03-12 11:59:49 +0100

    Initial Commit

---
+++ Oddsai/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Oddsai
+//
+//  Created by Nicolai Garder Hansen on 12/03/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Oddsai/OddsaiApp.swift
@@ -0,0 +1,17 @@
+//
+//  OddsaiApp.swift
+//  Oddsai
+//
+//  Created by Nicolai Garder Hansen on 12/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct OddsaiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
